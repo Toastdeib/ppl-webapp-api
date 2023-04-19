7a4bc66de6c2937b53b336afdc37c0fb10b3ccb1
Toastdeib <lord.xela@gmail.com>
2023-04-19 11:08:23 -0400

    Fixed accidental logger instantiation for test runs

--- logger.js
+++ logger.js
@@ -2,40 +2,58 @@ const winston = require('winston');
 require('winston-daily-rotate-file');
 const config = require('./config.js');
 
-const testRunLogger = {
-    debug: (msg) => {
-        console.log(`\x1b[36mD>\x1b[0m ${msg}`);
-    },
-    info: (msg) => {
-        console.log(`\x1b[32mI>\x1b[0m ${msg}`);
-    },
-    warn: (msg) => {
-        console.log(`\x1b[33mW>\x1b[0m ${msg}`);
-    },
-    error: (msg) => {
-        console.log(`\x1b[31mE>\x1b[0m ${msg}`);
-    },
+function dDebug(msg) {
+    console.log(`\x1b[36mD>\x1b[0m ${msg}`);
 }
 
-const apiLogger = winston.createLogger({
-    level: 'debug',
-    format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
-    transports: [
-        new winston.transports.DailyRotateFile({ filename: 'logs/api-error-%DATE%.log', level: 'error', maxFiles: '14d' }),
-        new winston.transports.DailyRotateFile({ filename: 'logs/api-combined-%DATE%.log', level: 'info', maxFiles: '14d' }),
-        new winston.transports.Console({ format: winston.format.simple() })
-    ]
-});
+function dInfo(msg) {
+    console.log(`\x1b[32mI>\x1b[0m ${msg}`);
+}
 
-const clientLogger = winston.createLogger({
-    format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
-    transports: [
-        new winston.transports.DailyRotateFile({ filename: 'logs/client-error-%DATE%.log', level: 'error', maxFiles: '14d' }),
-        new winston.transports.DailyRotateFile({ filename: 'logs/client-combined-%DATE%.log', level: 'info', maxFiles: '14d' }),
-    ]
-});
+function dWarn(msg) {
+    console.log(`\x1b[33mW>\x1b[0m ${msg}`);
+}
+
+function dError(msg) {
+    console.log(`\x1b[31mE>\x1b[0m ${msg}`);
+}
+
+let apiLogger, clientLogger;
+if (process.env.TEST_RUN === 'true') {
+    apiLogger = {
+        debug: dDebug,
+        info: dInfo,
+        warn: dWarn,
+        error: dError
+    };
+
+    clientLogger = {
+        debug: dDebug,
+        info: dInfo,
+        warn: dWarn,
+        error: dError
+    };
+} else {
+    apiLogger = winston.createLogger({
+        level: 'debug',
+        format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
+        transports: [
+            new winston.transports.DailyRotateFile({ filename: 'logs/api-error-%DATE%.log', level: 'error', maxFiles: '14d' }),
+            new winston.transports.DailyRotateFile({ filename: 'logs/api-combined-%DATE%.log', level: 'info', maxFiles: '14d' }),
+            new winston.transports.Console({ format: winston.format.simple() })
+        ]
+    });
+
+    clientLogger = winston.createLogger({
+        format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
+        transports: [
+            new winston.transports.DailyRotateFile({ filename: 'logs/client-error-%DATE%.log', level: 'error', maxFiles: '14d' }),
+            new winston.transports.DailyRotateFile({ filename: 'logs/client-combined-%DATE%.log', level: 'info', maxFiles: '14d' }),
+        ]
+    });
+}
 
 module.exports = {
-    api: process.env.TEST_RUN === 'true' ? testRunLogger : apiLogger,
+    api: apiLogger,
     client: clientLogger
 };
