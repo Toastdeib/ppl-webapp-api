09c9f65f9f804689c8d2775940f2be48ba3c094a
Toastdeib <lord.xela@gmail.com>
2023-06-05 14:20:56 -0400

    Added shell scripts to run the API and DB test suites in series

---
+++ tests/api.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEST_RUN=true TABLE_SUFFIX=_test node api-general.js
+echo "Tests complete; Press a to abort or any other key to continue..."
+read -s -n 1 k <&1
+if [[ $k = a ]] ; then
+exit 0
+fi
+echo
+TEST_RUN=true TABLE_SUFFIX=_test node api-challenger.js
+echo "Tests complete; Press a to abort or any other key to continue..."
+read -s -n 1 k <&1
+if [[ $k = a ]] ; then
+exit 0
+fi
+echo
+TEST_RUN=true TABLE_SUFFIX=_test node api-leader.js
---
+++ tests/db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEST_RUN=true TABLE_SUFFIX=_test node db-general.js
+echo "Tests complete; Press a to abort or any other key to continue..."
+read -s -n 1 k <&1
+if [[ $k = a ]] ; then
+exit 0
+fi
+echo
+TEST_RUN=true TABLE_SUFFIX=_test node db-challenger.js
+echo "Tests complete; Press a to abort or any other key to continue..."
+read -s -n 1 k <&1
+if [[ $k = a ]] ; then
+exit 0
+fi
+echo
+TEST_RUN=true TABLE_SUFFIX=_test node db-leader.js
